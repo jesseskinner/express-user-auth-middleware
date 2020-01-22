6186204311a3defc5d30e08426c6d48991a2f47f
Jesse Skinner <jesse@thefutureoftheweb.com>
2020-01-22 13:16:04 -0500

    NEW: create table, signup, getUserById all working fine

--- src/index.js
+++ src/index.js
@@ -1,7 +1,45 @@
-function UserAuthMiddleware({ query }) {
-    return {
-
+function UserAuthMiddleware({ tableName = 'users', database }) {
+    return { createTable, getUserById, signup };
+    
+    async function createTable() {
+        database.query(`
+            CREATE TABLE IF NOT EXISTS ${tableName} (
+                id INT(11) AUTO_INCREMENT NOT NULL,
+                email VARCHAR(255) NOT NULL,
+                password VARCHAR(255) NOT NULL,
+                created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+                PRIMARY KEY (id)
+            )
+        `)
     }
+
+	async function getUserById(id) {
+		const [users] = await database.query(
+			`
+                SELECT id, email
+                FROM ${tableName}
+                WHERE id = ?
+            `,
+			[id]
+        );
+        
+        return users[0];
+	}
+
+	async function signup({ email, password }) {
+		const res = await database.query(
+			`
+                INSERT INTO ${tableName}
+                SET ?
+            `,
+			{
+				email,
+				password
+			}
+        );
+        
+		return res[0].insertId;
+	}
 }
 
 export default UserAuthMiddleware;
--- test/signup.test.js
+++ test/signup.test.js
@@ -1,22 +1,39 @@
-import test from 'ava';
-import { query } from './_database.js';
+import { expect } from 'chai';
+import mysql from 'mysql2/promise';
 import UserAuthMiddleware from '../src/index.js';
 
-let auth;
+describe('express-user-auth-middleware', () => {
+	let auth, database;
 
-test.before(() => {
-	auth = new UserAuthMiddleware({
-		query
+	before(async () => {
+		database = await mysql.createConnection({
+			host: '127.0.0.1',
+			user: 'root',
+			database: 'test_express_user_auth_middleware'
+		});
+
+		auth = new UserAuthMiddleware({
+			database
+		});
+
+		auth.createTable();
 	});
-});
 
-test('signup should create a user with an email address and password', async t => {
-	const email = 'abc@example.com';
-	const password = 'abc';
+	after(async () => {
+		await database.end();
+	});
 
-	const user = await auth.signup({ email, password });
+	describe('signup', () => {
+		it('should create a user with an email address and password', async () => {
+			const email = 'abc@example.com';
+			const password = 'abc';
 
-	t.is(user.email, email);
-	t.undefined(user.password);
-	t.greaterThan(user.id, 0);
+			const id = await auth.signup({ email, password });
+			const user = await auth.getUserById(id);
+
+			expect(user.email).to.deep.equal(email);
+			expect(user.password).to.be.undefined;
+			expect(user.id).to.be.greaterThan(0);
+		});
+	});
 });
